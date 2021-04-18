b6451d0ad0843804fc9d0c6ce203c04c4561566d
PrashanthBhonagiri <prashanth.bhonagiri@gmail.com>
2021-04-18 14:27:54 +0530

    creating transactions

--- chain-util.js
+++ chain-util.js
@@ -1,10 +1,16 @@
 const EC = require('elliptic').ec;
+const {v1 : uuidV1} = require('uuid');
+//for unique id absed on timestamp(v1)
+
 const ec = new EC('secp256k1'); 
 //secp => standards of efficient/official cryptography prime 256 bits
 
 class ChainUtil {
     static genKeyPair() {
         return ec.genKeyPair();
+    }
+    static id() {
+        return uuidV1();
     }
 }
 
