563a78fbd00d04f190ac15034b62048babefacbf
PrashanthBhonagiri <prashanth.bhonagiri@gmail.com>
2021-04-29 14:15:44 +0530

    added mine-transactions endpoint

--- app/index.js
+++ app/index.js
@@ -7,12 +7,14 @@ const Blockchain = require('../blockchain/index');
 const P2pServer = require('./p2p-server');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
+const Miner = require('./miner');
 
 const app = express();
 const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc, tp);
+const miner = new Miner(bc, tp, wallet, p2pServer);
 
 app.use(morgan('dev'));
 app.use(cors());
@@ -51,6 +53,12 @@ app.post('/transact', (req, res) => {
     p2pServer.broadcastTransaction(transaction);
     
     res.redirect('/transactions');
+});
+
+app.get('/mine-transactions',(req,res) => {
+    const block = miner.mine();
+    console.log(`New Block added : ${block.toString()}`);
+    res.redirect('/blocks');
 });
 
 app.get('/public-key', (req, res) => {
