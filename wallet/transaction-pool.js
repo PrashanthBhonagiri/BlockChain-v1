f41bc660c9b8f320ff317515cf93d28a0da997c2
PrashanthBhonagiri <prashanth.bhonagiri@gmail.com>
2021-04-29 12:51:51 +0530

    logs added for t-pool

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -5,10 +5,13 @@ class TransactionPool {
   
     updateOrAddTransaction(transaction) {
       let transactionWithId = this.transactions.find(t => t.id === transaction.id);
+      console.log(transactionWithId);
       if (transactionWithId) {
         this.transactions[this.transactions.indexOf(transactionWithId)] = transaction;
+        console.log("updated", this.transactions);
       } else {
         this.transactions.push(transaction);
+        console.log("added new " , this.transactions);
       }
     }
     existingTransaction(address) {
